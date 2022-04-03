4185ba531e581ff83a6abdb642fdc189f27562de
jorgelerre <92641255+jorgelerre@users.noreply.github.com>
2022-04-03 15:32:16 +0200

    Algoritmo hecho (falta depurar)

--- practica2/acciones_DyV.cpp
+++ practica2/acciones_DyV.cpp
@@ -22,14 +22,15 @@ using namespace std;
 */
 int MejorCompraVentaDyV(const vector<int> & v, int ini, int fin, int & dia_compra, int & dia_venta, int & min, int & max){
   //Si el tamaño del trozo de vector es 3 o menos, operamos sobre el tramo
+  int solucion = INT_MIN;
   if(fin - ini < 2){
-    max = min = v[ini];
+    max = min = ini;
     //Calculamos max y min
     for(int i = ini+1; i < fin; i++){
-      if(v[i] > max)
-        max = v[i];
-      else if(v[i] < min)
-        min = v[i];
+      if(v[i] > v[max])
+        max = i;
+      else if(v[i] < v[min])
+        min = i;
     }
     //Calculamos ganancia, dia_compra y dia_venta
     for(int i = ini; i < fin-1; i++){     
@@ -46,14 +47,37 @@ int MejorCompraVentaDyV(const vector<int> & v, int ini, int fin, int & dia_compr
   }
   else{
     int dia_compra_izq, dia_compra_dcha, dia_venta_izq, dia_venta_dcha, min_izq, min_dcha, max_izq, max_dcha, media;
+    int ganancia_izq, ganancia_dcha, ganancia_comb, sol;
     //Dividimos el vector dado en dos trozos (lo más iguales posible)
     media = (fin+ini)/2 + 1;
     //Llamamos a la función de forma recursiva
-    MejorCompraVentaDyV(v, ini, media, dia_compra_izq, dia_venta_izq, min_izq, max_izq);
-    MejorCompraVentaDyV(v, media, fin, dia_compra_dcha, dia_venta_dcha, min_dcha, max_dcha);
+    ganancia_izq  = MejorCompraVentaDyV(v, ini, media, dia_compra_izq,  dia_venta_izq,  min_izq,  max_izq );
+    ganancia_dcha = MejorCompraVentaDyV(v, media, fin, dia_compra_dcha, dia_venta_dcha, min_dcha, max_dcha);
     //Combinamos soluciones
     
+    min = (min_izq < min_dcha) ? min_izq : min_dcha;
+    max = (max_izq > max_dcha) ? max_izq : max_dcha;
+   
+    ganancia_comb = v[max_dcha] - v[min_izq];
+    if(ganancia_comb > ganancia_izq && ganancia_comb > ganancia_dcha){
+      solucion = ganancia_comb;
+      dia_compra = min_izq;
+      dia_venta = max_dcha;
+    }
+    else if(ganancia_izq > ganancia_dcha){
+      solucion = ganancia_izq;
+      dia_compra = dia_compra_izq;
+      dia_venta = dia_venta_izq;
+    }
+    else{
+      solucion = ganancia_dcha;
+      dia_compra = dia_compra_dcha;
+      dia_venta = dia_venta_dcha;
+    }
+   
   }
+  
+  return solucion;
 }
 
 int MejorCompraVentaDyV(const vector<int> & v, int & dia_compra, int & dia_venta){
@@ -68,7 +92,7 @@ int main(){
 	
 	vector<int> v = {1, 2, 300, 4, 15, 0, 7, 20, 9, 0};
 	
-	solucion = MejorCompraVentaBasico(v, dia_compra, dia_venta);
+	solucion = MejorCompraVentaDyV(v, dia_compra, dia_venta);
 	
 	cout << "Solucion: " << endl;
 	cout << "Elemento minimo -> v[" << min << "]= " << v[min]
